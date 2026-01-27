c2fde331f38e5f1bf16cbf6268e17d8cbc5fbfb2
Чащин Артем <chashchin.am@phystech.edu>
2026-01-28 00:42:06 +0500

    fix base notification schema

--- schemas/notifications.py
+++ schemas/notifications.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class BaseNotificationSchema(BaseModel):
-    message: str
+    message: str = Field(min_length=1)
 
 
 class CreateNotificationSchema(BaseNotificationSchema):
